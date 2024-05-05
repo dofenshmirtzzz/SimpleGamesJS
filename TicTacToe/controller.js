581da2d7c4965ed51ba37d1704d2563e50caca96
estebansg1 <estebandesa0@gmail.com>
2024-05-05 14:45:30 -0600

    fix tictactoe

--- TicTacToe/controller.js
+++ TicTacToe/controller.js
@@ -2,140 +2,70 @@ let turno = 1;
 let fichas = ["O", "X"];
 let puestas = 0;
 let partidaAcabada = false;
-let textoVictoria = 
-	document.getElementById("textoVictoria");
-let botones = 
-	Array.from(document.getElementsByTagName("button"));
+let textVictory = document.getElementById("textVictory");
+let botones = Array.from(document.getElementsByTagName("button"));
 
-botones.forEach(
-	x => x.addEventListener("click", ponerFicha)
-);
+botones.forEach(button => button.addEventListener("click", ponerFicha));
 
-function ponerFicha(event){
-	let botonPulsado = event.target;
-	if(!partidaAcabada && botonPulsado.innerHTML == ""){
-		botonPulsado.innerHTML = fichas[turno];
-		puestas += 1;
-		
-		let estadoPartida = estado();
-		if(estadoPartida == 0){
-			cambiarTurno();
-			if(puestas < 9){
-				ia();
-				estadoPartida = estado();
-				puestas += 1;
-				cambiarTurno();	
-			}	
-		}
-		
-		if(estadoPartida == 1){
-			textoVictoria.style.visibility = "visible";
-			partidaAcabada = true;
-		}
-		else if(estadoPartida == -1){
-			textoVictoria.innerHTML = "Has perdido ;("
-			partidaAcabada = true;
-			textoVictoria.style.visibility = "visible";
-		}
-	}	
+function ponerFicha(event) {
+    let botonPulsado = event.target;
+    if (!partidaAcabada && botonPulsado.innerHTML == "") {
+        botonPulsado.innerHTML = fichas[turno];
+        puestas += 1;
+        
+        let estadoPartida = estado();
+        if (estadoPartida == 0) {
+            cambiarTurno();
+            if (puestas < 9) {
+                ia();
+                estadoPartida = estado();
+                puestas += 1;
+                cambiarTurno();    
+            }   
+        }
+        
+        if (estadoPartida == 1 || estadoPartida == -1) {
+            let mensaje = estadoPartida == 1 ? "You win! :)" : "You lost :(";
+            textVictory.innerHTML = mensaje;
+            textVictory.style.visibility = "visible";
+            partidaAcabada = true;
+            mostrarRecargarPagina();
+        }
+    }   
 }
 
-function cambiarTurno(){
-	if(turno==1){
-		turno = 0;
-	}
-	else{
-		turno = 1;
-	}
-	/*
-		Otra forma de hacerlo:
-		turno += 1;
-		turno %= 2;
-	*/
+function cambiarTurno() {
+    turno = turno === 1 ? 0 : 1;
 }
 
-function estado(){
-	posicionVictoria = 0;
-	nEstado = 0;
+function estado() {
+    let lineasGanadoras = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // filas
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // columnas
+        [0, 4, 8], [2, 4, 6]             // diagonales
+    ];
 
-	function sonIguales(...args){
-		valores = args.map(x=>x.innerHTML);
-		if(valores[0] != "" && valores.every((x, i, arr) => x===arr[0])){
-			args.forEach(x => x.style.backgroundColor = "Fuchsia")
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-
-	//Comprobamos si hay alguna linea
-	if(sonIguales(botones[0], botones[1], botones[2])){
-		posicionVictoria = 1;
-	}
-
-	else if(sonIguales(botones[3], botones[4], botones[5])){
-		posicionVictoria = 2;
-	}
-
-	else if(sonIguales(botones[6], botones[7], botones[8])){
-		posicionVictoria = 3;
-	}
-
-	else if(sonIguales(botones[0], botones[3], botones[6])){
-		posicionVictoria = 4;
-	}
-
-	else if(sonIguales(botones[1], botones[4], botones[7])){
-		posicionVictoria = 5;
-	}
-
-	else if(sonIguales(botones[2], botones[5], botones[8])){
-		posicionVictoria = 6;
-	}
-
-	else if(sonIguales(botones[0], botones[4], botones[8])){
-		posicionVictoria = 7;
-	}
-
-	else if(sonIguales(botones[2], botones[4], botones[6])){
-		posicionVictoria = 8;
-	}
-
-	//Comprobamos quien ha ganado
-	if(posicionVictoria > 0){
-		if(turno == 1){
-			nEstado = 1;
-		}
-		else{
-			nEstado = -1;
-		}
-	}
-
-	return nEstado;
+    for (let linea of lineasGanadoras) {
+        let [a, b, c] = linea;
+        if (botones[a].innerHTML !== "" &&
+            botones[a].innerHTML === botones[b].innerHTML &&
+            botones[a].innerHTML === botones[c].innerHTML) {
+            botones[a].style.backgroundColor = "Fuchsia";
+            botones[b].style.backgroundColor = "Fuchsia";
+            botones[c].style.backgroundColor = "Fuchsia";
+            return botones[a].innerHTML === fichas[1] ? 1 : -1;
+        }
+    }
+    return puestas === 9 ? 2 : 0;
 }
 
-function ia(){
-	function aleatorio(min, max) {
-  		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
+function ia() {
+    let valores = botones.map(button => button.innerHTML);
+    let pos = valores.indexOf("");
 
-	let valores = botones.map(x=>x.innerHTML);
-	let pos = -1;
+    if (pos === -1) return;
 
-	if(valores[4]==""){
-		pos = 4;
-	}
-	else{
-		let n = aleatorio(0, botones.length-1);
-		while(valores[n]!=""){
-			n = aleatorio(0, botones.length-1); 
-		}
-		pos = n;
-	}
-
-	botones[pos].innerHTML = "O";
-	return pos;
+    botones[pos].innerHTML = "O";
 }
 
 function mostrarRecargarPagina() {
@@ -146,36 +76,4 @@ function mostrarRecargarPagina() {
 function ocultarRecargarPagina() {
     let recargarPagina = document.getElementById("recargarPagina");
     recargarPagina.style.display = "none";
-}
-
-function ponerFicha(event){
-    let botonPulsado = event.target;
-    if(!partidaAcabada && botonPulsado.innerHTML == ""){
-        botonPulsado.innerHTML = fichas[turno];
-        puestas += 1;
-        
-        let estadoPartida = estado();
-        if(estadoPartida == 0){
-            cambiarTurno();
-            if(puestas < 9){
-                ia();
-                estadoPartida = estado();
-                puestas += 1;
-                cambiarTurno();    
-            }   
-        }
-        
-        if(estadoPartida == 1){
-            textoVictoria.innerHTML = "¡Has ganado! :)";
-            textoVictoria.style.visibility = "visible";
-            partidaAcabada = true;
-            mostrarRecargarPagina();
-        }
-        else if(estadoPartida == -1){
-            textoVictoria.innerHTML = "¡Has perdido! :(";
-            textoVictoria.style.visibility = "visible";
-            partidaAcabada = true;
-            mostrarRecargarPagina();
-        }
-    }   
 }
